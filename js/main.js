59edb41ba917ca845ab61bfb650bf99f2e4bd7e8
stegmannoah@gmail.com <stegmannoah@gmail.com>
2022-05-29 11:11:04 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+	anchors:['home', 'details', 'us', 'rsvp'],
+	autoScrolling:true,
+	scrollingSpeed: 600,
+	scrollHorizontally: true,
+	loopBottom: true,
+	navigation: true,
+	parallax: true,
+});
