582fd395db5b5c8f3ad1f2252d879ac9a521ac5c
stegmannoah@gmail.com <stegmannoah@gmail.com>
2022-05-29 13:39:59 -0700

    responsiveScrolling set to 1000

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ new fullpage('#fullpage', {
 	anchors:['home', 'details', 'us', 'rsvp'],
 	autoScrolling:true,
 	scrollingSpeed: 600,
+	responsiveWidth: 1000,
 	scrollHorizontally: true,
 	loopBottom: true,
 	navigation: true,
